8081cd666751e2ec361f8ffc5f94b9640a485592
Valgoi <luis.valgoi@sap.com>
2020-12-19 18:14:44 -0300

    feat: added irst version of the login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { StyledContainer, StyledContent } from "./style";
+import { useAuth } from "../../contexts/auth";
 
 export default function Login() {
+  const { signIn, loading } = useAuth();
+
   return (
     <>
       <Helmet title="FreeTeuxDeux - Login" />
-      <div>Login</div>
+      <StyledContainer>
+        <StyledContent>
+          <button onClick={() => signIn()}>{loading ? "Loading..." : "LogIn"}</button>
+        </StyledContent>
+      </StyledContainer>
     </>
   );
 }
---
+++ src/pages/Login/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledContent = styled.div`
+  width: auto;
+`;
