810d927e4c748f0826553075ef5b569ac35534b9
Valgoi <luis.valgoi@sap.com>
2020-12-19 13:14:39 -0300

    feat: added logo atom

--- src/atoms/Logo/index.js
+++ src/atoms/Logo/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { CONSTANTS as RoutesPath } from "../../routes/paths";
+import { StyledLogo } from "./style";
 
 export default function index() {
-  return <div>Logo</div>;
+  return (
+    <Link to={RoutesPath.HOME.path}>
+      <StyledLogo src="/images/logo.png" alt="logo" />
+    </Link>
+  );
 }
---
+++ src/atoms/Logo/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledLogo = styled.img`
+  max-width: 3rem;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: all 0.2s;
+  }
+`;
