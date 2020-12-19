4e08787f0719c71149352985a614b44a511a25b8
Valgoi <luis.valgoi@sap.com>
2020-12-19 17:59:38 -0300

    refator: replace to for onclick

--- src/atoms/SettingsListItem/index.js
+++ src/atoms/SettingsListItem/index.js
@@ -3,11 +3,11 @@ import { useHistory } from "react-router-dom";
 
 import { StyledSettingsListItem } from "./style";
 
-export default function SettingsListItem({ text, to }, props) {
+export default function SettingsListItem({ text, onClick }, props) {
   const history = useHistory();
 
   return (
-    <StyledSettingsListItem {...props} onClick={() => history.push(to)}>
+    <StyledSettingsListItem {...props} onClick={() => onClick(history)}>
       {text}
     </StyledSettingsListItem>
   );
--- src/molecules/SettingsList/index.js
+++ src/molecules/SettingsList/index.js
@@ -2,31 +2,42 @@ import React from "react";
 
 import { CONSTANTS as RoutesPath } from "../../routes/paths";
 import { StyledSettingsList } from "./style";
+import { useAuth } from "../../contexts/auth";
 import SettingsListItem from "../../atoms/SettingsListItem";
 
-const SETTINGS_ITEMS = [
-  {
-    key: RoutesPath.ACCOUNT.key,
-    path: RoutesPath.ACCOUNT.path,
-    text: "Account",
-  },
-  {
-    key: RoutesPath.DIGEST.key,
-    path: RoutesPath.DIGEST.path,
-    text: "Daily Digest",
-  },
-  {
-    key: RoutesPath.COLORS.key,
-    path: RoutesPath.COLORS.path,
-    text: "Colors",
-  },
-];
-
 export default function SettingsList(props) {
+  const { signOut } = useAuth();
+
+  const SETTINGS_ITEMS = [
+    {
+      key: RoutesPath.ACCOUNT.key,
+      onClick: (history) => history.push(RoutesPath.ACCOUNT.path),
+      text: "Account",
+    },
+    {
+      key: RoutesPath.DIGEST.key,
+      onClick: (history) => history.push(RoutesPath.DIGEST.path),
+      text: "Daily Digest",
+    },
+    {
+      key: RoutesPath.COLORS.key,
+      onClick: (history) => history.push(RoutesPath.COLORS.path),
+      text: "Colors",
+    },
+    {
+      key: "logout",
+      onClick: (history) => {
+        signOut();
+        history.push(RoutesPath.HOME.path);
+      },
+      text: "Logout",
+    },
+  ];
+
   return (
     <StyledSettingsList {...props}>
       {SETTINGS_ITEMS.map((item) => {
-        return <SettingsListItem key={item.key} text={item.text} to={item.path} />;
+        return <SettingsListItem key={item.key} text={item.text} onClick={item.onClick} />;
       })}
     </StyledSettingsList>
   );
