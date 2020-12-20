5711e1d281e2d79f69e453a2995fdec2da5626fa
Valgoi <luis.valgoi@sap.com>
2020-12-20 17:07:37 -0300

    feat: added listheader

--- src/molecules/ListHeader/index.js
+++ src/molecules/ListHeader/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { StyledListHeader } from "./style";
+import Title from "../../atoms/Title";
+import SubTitle from "../../atoms/SubTitle";
 
-export default function ListHeader() {
-  return <div>ListHeader</div>;
+export default function ListHeader({ title, subtitle }) {
+  return (
+    <StyledListHeader>
+      <Title text={title} />
+      <SubTitle text={subtitle} />
+    </StyledListHeader>
+  );
 }
---
+++ src/molecules/ListHeader/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledListHeader = styled.div`
+  display: flex;
+  height: auto;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`;
