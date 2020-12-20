9fc0516985a9ba870ea5a75abfbea788d0b5a73e
Valgoi <luis.valgoi@sap.com>
2020-12-19 22:49:17 -0300

    feat: added title

---
+++ src/atoms/Title/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledTitle = styled.h2`
+  font-family: TitleTX, -apple-system, system-ui, sans-serif;
+  font-size: 60px;
+  line-height: 66px;
+  font-weight: normal;
+  text-transform: uppercase;
+  margin: 0;
+`;
