8563217b43dbfe2b782431ca0669408c2b9ccc45
Valgoi <luis.valgoi@sap.com>
2020-12-19 22:44:10 -0300

    refactor: aded src dinamic change

--- src/atoms/Logo/index.js
+++ src/atoms/Logo/index.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 import { CONSTANTS as RoutesPath } from "../../routes/paths";
 import { StyledLogo } from "./style";
 
-export default function Logo() {
+export default function Logo({ src }) {
   return (
     <Link to={RoutesPath.HOME.path}>
-      <StyledLogo src="/images/logo.png" alt="logo" />
+      <StyledLogo src={`/images/${src ? src : "logo"}.png`} alt="logo" />
     </Link>
   );
 }
