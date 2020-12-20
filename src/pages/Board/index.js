eb24906576fe054f54d89cb153e4799e1f43129f
Valgoi <luis.valgoi@sap.com>
2020-12-20 17:41:45 -0300

    feat: applied list to the board

--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import List from "../../molecules/List";
 import BoardTemplate from "../../templates/Board";
 
 export default function Board() {
@@ -9,16 +10,20 @@ export default function Board() {
       <BoardTemplate
         primary={
           <>
-            <h1>h1</h1>
-            <h1>h1</h1>
-            <h1>h1</h1>
-            <h1>h1</h1>
-            <h1>h1</h1>
-            <h1>h1</h1>
-            <h1>h1</h1>
+            <List title="Domingo" subtitle="20 de Dezembro de 2020" />
+            <List title="Segunda" subtitle="20 de Dezembro de 2020" />
+            <List title="Terça" subtitle="20 de Dezembro de 2020" />
+            <List title="Quarta" subtitle="20 de Dezembro de 2020" />
+            <List title="Quinta" subtitle="20 de Dezembro de 2020" />
+            <List title="Sexta" subtitle="20 de Dezembro de 2020" />
+            <List last title="Sabado" subtitle="20 de Dezembro de 2020" />
           </>
         }
-        secondary={<h1>h2</h1>}
+        secondary={
+          <>
+            <h1>h2</h1>
+          </>
+        }
       ></BoardTemplate>
     </>
   );
--- src/templates/Board/style.js
+++ src/templates/Board/style.js
@@ -1,22 +1,26 @@
 import styled from "styled-components";
 
 export const StyledMainSection = styled.div`
-  height: 100%;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+  height: 100%;
 `;
 
 export const StyledPrimarySection = styled.div`
+  height: 50%;
   display: flex;
   flex-wrap: wrap;
   flex-grow: 1;
   flex-direction: row;
+  overflow: auto;
 `;
 
 export const StyledSecondarySection = styled.div`
+  height: 50%;
   display: flex;
   flex-wrap: wrap;
   flex-grow: 1;
   flex-direction: row;
+  overflow: auto;
 `;
