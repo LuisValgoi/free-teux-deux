d73db4dc858ec5e4b5659039c0cff2ef02eb3200
Valgoi <luis.valgoi@sap.com>
2020-12-21 15:15:45 -0300

    refactor: reduced margin navicon

--- src/atoms/NavIcon/style.js
+++ src/atoms/NavIcon/style.js
@@ -4,7 +4,7 @@ import IconBase from "../IconBase";
 import * as Colors from "../../styles/colors";
 
 export const StyledNavIcon = styled(IconBase)`
-  margin-top: 1rem;
+  margin-top: 0.5rem;
   font-size: ${(props) => (props.primary ? "3rem" : "1rem")};
   color: ${(props) => (props.primary ? Colors.NAV_ICON_COLOR_PRIMARY : Colors.NAV_ICON_COLOR)};
 `;
