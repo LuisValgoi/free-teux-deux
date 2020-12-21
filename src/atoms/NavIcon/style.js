e8364b9b7f27f4dd9dfffd97624f120a28f50c67
Valgoi <luis.valgoi@sap.com>
2020-12-21 12:54:30 -0300

    feat: added navicon button

---
+++ src/atoms/NavIcon/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import IconBase from "../IconBase";
+
+import * as Colors from "../../styles/colors";
+
+export const StyledNavIcon = styled(IconBase)`
+  margin-top: 1rem;
+  font-size: ${(props) => (props.primary ? "3rem" : "1rem")};
+  color: ${(props) => (props.primary ? Colors.NAV_ICON_COLOR_PRIMARY : Colors.NAV_ICON_COLOR)};
+`;
